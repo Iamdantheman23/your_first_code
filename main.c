d94f728b198232c59e2a7380ff5bb703ddc3aaef
Iamdantheman23 <130517112+Iamdantheman23@users.noreply.github.com>
2023-04-11 20:28:42 -0400

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,33 @@ int main()
   int i;
   for (i = 0; i < 10; i++)
   {
-    printf("School\n");
+    printf("Hello School\n");
   }
   return 0;
 }
+int main()
+{
+  int i;
+  for (i = 0; i < 10; i++)
+  {
+    printf("Hello School/n")
+           }
+           return 0;
+           } 
+           int main()
+           {
+             int i;
+             for (i = 0 < 10; i++)
+             {
+               printf("Hello School/n);
+                      }
+                      return 0;
+                      } 
+                      int main()
+                      { 
+                        int i;
+                        for ( i = 0; i < 10; i++
+                             print f(Hello School/n");
+                                     } 
+                                     return 0;
+                                     }
